423d261683c8ac233156b6be35cfe833adcd3230
Duck <Duck123321@yandex.ru>
2022-03-29 21:51:56 +0300

    Добавился ноый css файл

--- Project/settings.py
+++ Project/settings.py
@@ -4,4 +4,5 @@ work_dir = 'Project'
 
 path_db = os.path.join(os.getcwd(), 'Project', 'db', 'db.db')
 
+# set FLASK_APP=Project/main.py
 # python -m flask run --host localhost --port 8080 --reload --debugger
